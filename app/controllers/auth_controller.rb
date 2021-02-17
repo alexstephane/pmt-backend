1a10f716a10ecb860f3aa198b57e308e437b45c9
github username <alexngundji@gmail.com>
2021-02-17 12:34:04 -0500

    clean up

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,33 +1,4 @@
 
-# class AuthController < ApplicationController
-
-#   def create
-
-#     if params[:title] === 'manager' 
-#        @user = Manager.find_by(username: params[:username])
-#       else
-#         @user = Salesrep.find_by(username: params[:username])
-#     end
-
-#     if @user && @user.authenticate(params[:password])
-#       token = encode("id": @user.id)
-#       render json: 
-#       {
-#         authenticated: true,
-#         message: "login success",
-#         token: token,
-#         user: @user,
-#       }, status: :accepted
-#     else
-#         render json: {
-#         authenticated: false,
-#         message: "login failed"
-#       }, status: :unauthorized 
-#   end
-  
-  
-# end
-# end
 
 class AuthController < ApplicationController
 
--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -1,5 +1,4 @@
 class Prospect < ApplicationRecord
-    # belongs_to :manager 
-    # belongs_to :salesrep
+    
     has_many :activities
 end
